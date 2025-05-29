0c062fcda0ba00ef8afd609025fbfeed4909d2fa
vincent <daylightzone91@gmail.com>
2025-05-29 20:13:11 +0700

    update design

--- resources/views/Admin/user/index.blade.php
+++ resources/views/Admin/user/index.blade.php
@@ -4,26 +4,26 @@
     <!-- Start Main Content Area -->
     <div class="container-fluid">
             <div class="main-content-container overflow-hidden">
-{{--                <div class="d-flex justify-content-between align-items-center flex-wrap gap-2 mb-4">--}}
-{{--                    <h3 class="mb-0">Transaction</h3>--}}
+                <div class="d-flex justify-content-between align-items-center flex-wrap gap-2 mb-4">
+                    <h3 class="mb-0">Transaction</h3>
 
-{{--                    <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">--}}
-{{--                        <ol class="breadcrumb align-items-center mb-0 lh-1">--}}
-{{--                            <li class="breadcrumb-item">--}}
-{{--                                <a href="#" class="d-flex align-items-center text-decoration-none">--}}
-{{--                                    <i class="ri-home-4-line fs-18 text-primary me-1"></i>--}}
-{{--                                    <span class="text-secondary fw-medium hover">Dashboard</span>--}}
-{{--                                </a>--}}
-{{--                            </li>--}}
-{{--                            <li class="breadcrumb-item active" aria-current="page">--}}
-{{--                                <span class="fw-medium">Finance</span>--}}
-{{--                            </li>--}}
-{{--                            <li class="breadcrumb-item active" aria-current="page">--}}
-{{--                                <span class="fw-medium">Transaction</span>--}}
-{{--                            </li>--}}
-{{--                        </ol>--}}
-{{--                    </nav>--}}
-{{--                </div>--}}
+                    <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+                        <ol class="breadcrumb align-items-center mb-0 lh-1">
+                            <li class="breadcrumb-item">
+                                <a href="#" class="d-flex align-items-center text-decoration-none">
+                                    <i class="ri-home-4-line fs-18 text-primary me-1"></i>
+                                    <span class="text-secondary fw-medium hover">Dashboard</span>
+                                </a>
+                            </li>
+                            <li class="breadcrumb-item active" aria-current="page">
+                                <span class="fw-medium">Finance</span>
+                            </li>
+                            <li class="breadcrumb-item active" aria-current="page">
+                                <span class="fw-medium">Transaction</span>
+                            </li>
+                        </ol>
+                    </nav>
+                </div>
 
                 <div class="card bg-white border-0 rounded-3 mb-4">
                     <div class="card-body p-0">
@@ -34,13 +34,17 @@
                                     <i class="ri-add-line d-none d-sm-inline-block"></i>
                                     <span>Add Transaction</span>
                                 </button>
-                                <select class="form-select month-select form-control w-135 bg-border-color border-color bg-transparent" aria-label="Default select example">
-                                    <option selected="">Last 30 days</option>
-                                    <option value="1">Last 90 days</option>
-                                    <option value="1">Last 1 year</option>
+                                <form class="position-relative table-src-form me-0">
+                                    <input type="text" class="form-control rounded-2" placeholder="Search here....">
+                                    <i class="material-symbols-outlined position-absolute top-50 start-0 translate-middle-y text-secondary">search</i>
+                                </form>
+                                <select class="form-select rounded-2 month-select form-control w-135 bg-border-color border-color bg-transparent" aria-label="Default select example">
+                                    <option selected=""> 30 days</option>
+                                    <option value="1"> 90 days</option>
+                                    <option value="1"> 1 year</option>
                                 </select>
                             </div>
-                        </div>
+                            </div>
 
                         <div class="default-table-area style-two transaction-history-large">
                             <div class="table-responsive">
@@ -395,96 +399,7 @@
                 </div>
 
                 <!-- Modal Card Add -->
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered" style="max-width: 1120px;">
-                        <div class="modal-content p-4 p-md-5">
-                            <div class="modal-header p-0 border-0">
-                                <h3 class="mb-0">Add A New Transaction</h3>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body p-0 pt-4">
-                                <form action="">
-                                    <div class="row">
-                                        <div class="col-sm-6">
-                                            <div class="mb-4">
-                                                <label class="label text-secondary">Transaction ID</label>
-                                                <input type="text" class="form-control h-55" placeholder="Enter name">
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <div class="mb-4">
-                                                <label class="label text-secondary">Date</label>
-                                                <input type="date" class="form-control h-55">
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <div class="mb-4">
-                                                <label class="label text-secondary">Description</label>
-                                                <input type="text" class="form-control h-55" placeholder="Enter Transaction Description">
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <div class="mb-4">
-                                                <label class="label text-secondary">Amount</label>
-                                                <input type="number" class="form-control h-55" placeholder="Enter Amount">
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <div class="mb-4">
-                                                <label class="label text-secondary">Type</label>
-                                                <select class="form-select form-control h-55" aria-label="Default select example">
-                                                    <option selected>Select Type: Income/Expense</option>
-                                                    <option value="1">Income</option>
-                                                    <option value="2">Expense</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <div class="mb-4">
-                                                <label class="label text-secondary">Status</label>
-                                                <select class="form-select form-control h-55" aria-label="Default select example">
-                                                    <option selected>Select Type: Completed/Pending</option>
-                                                    <option value="1">Completed</option>
-                                                    <option value="2">Pending</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <div class="mb-4">
-                                                <label class="label text-secondary">Payment Method</label>
-                                                <input type="text" class="form-control h-55" placeholder="Enter Payment Method: Bank Transfer, Cash, Card">
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <div class="mb-4">
-                                                <label class="label text-secondary">Action</label>
-                                                <select class="form-select form-control h-55" aria-label="Default select example">
-                                                    <option selected>Select Type: Action</option>
-                                                    <option value="1">View</option>
-                                                    <option value="2">Edit</option>
-                                                    <option value="2">Delete</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-12">
-                                            <div class="mb-4">
-                                                <label class="label text-secondary">Notes <span class="text-body fw-normal">[Optional Notes]</span></label>
-                                                <textarea rows="5" class="form-control" placeholder="Write any additional note here..."></textarea>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                            <div class="modal-footer border-0 p-0 gap-2">
-                                <button type="button" class="btn btn-danger text-white px-3 m-0" data-bs-dismiss="modal">Cancel</button>
-                                <button type="button" class="btn btn-primary hover-bg px-3 m-0">
-                                    <i class="ri-add-line"></i>
-                                    <span>Add A Transaction</span>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @include('Admin.user.create')
             </div>
 
             <div class="flex-grow-1"></div>
